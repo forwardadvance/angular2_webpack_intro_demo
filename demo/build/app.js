f76f055bcac929ae085ae0a2027c5dd81c83855b
Nicholas Johnson <nicholas@forwardadvance.com>
2016-11-16 16:08:28 +0000

    first commit

---
+++ demo/build/app.js
@@ -0,0 +1,71 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+/******/
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			exports: {},
+/******/ 			id: moduleId,
+/******/ 			loaded: false
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.loaded = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/*!***********************!*\
+  !*** ./source/app.js ***!
+  \***********************/
+/***/ function(module, exports, __webpack_require__) {
+
+	var cat = __webpack_require__(/*! ./cat */ 1)
+	
+	console.log(cat.miow());
+
+
+/***/ },
+/* 1 */
+/*!***********************!*\
+  !*** ./source/cat.js ***!
+  \***********************/
+/***/ function(module, exports) {
+
+	module.exports = {
+	  miow: function() {
+	    return 'Woof';
+	  }
+	};
+
+
+/***/ }
+/******/ ]);
+//# sourceMappingURL=app.js.map
---
+++ demo/source/cat.js
@@ -0,0 +1,5 @@
+module.exports = {
+  miow: function() {
+    return 'Woof';
+  }
+};
